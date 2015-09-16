af8fe6cb3400ab5a6ba09a7a535306f0b3a0f682
aldous <aldous@adobe.com>
2015-09-15 18:02:29 -0600

    Updated App

--- my-app/src/main/java/org/aldous/app/App.java
+++ my-app/src/main/java/org/aldous/app/App.java
@@ -4,10 +4,10 @@ package org.aldous.app;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello, World! This is Jim." );
     }
 }
